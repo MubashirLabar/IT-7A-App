a8e740af78da284c6d5ac48d0f6fe19fba067790
MubashirLabar <mubashirnazir115@gmail.com>
2025-10-30 13:50:53 +0500

    image slider

--- App.js
+++ App.js
@@ -1,18 +1,42 @@
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, Image, ScrollView, Dimensions } from "react-native";
+
+// const width = Dimensions.get("window").width;
+// const height = Dimensions.get("window").height;
+
+const { width, height } = Dimensions.get("window");
 
 function App() {
   return (
-    <ScrollView horizontal={true}>
+    <ScrollView>
       <View style={styles.container}>
-        <View style={[styles.box, { backgroundColor: "red" }]}>
-          <Text style={styles.title}>Pizza</Text>
-        </View>
-        <View style={[styles.box, { backgroundColor: "orange" }]}>
-          <Text style={styles.title}>Zinger</Text>
-        </View>
-        <View style={[styles.box, { backgroundColor: "blue" }]}>
-          <Text style={styles.title}>Pasta</Text>
-        </View>
+        <Image source={require("./images/img-1.jpg")} style={styles.image} />
+
+        <Image
+          source={{
+            uri: "https://images.pexels.com/photos/34341415/pexels-photo-34341415.jpeg",
+          }}
+          style={styles.image}
+        />
+
+        <Image
+          source={{
+            uri: "https://images.pexels.com/photos/34101421/pexels-photo-34101421.jpeg",
+          }}
+          style={styles.image}
+        />
+
+        <Image
+          source={{
+            uri: "https://images.pexels.com/photos/34369620/pexels-photo-34369620.jpeg",
+          }}
+          style={styles.image}
+        />
+        <Image
+          source={{
+            uri: "https://images.pexels.com/photos/8428267/pexels-photo-8428267.jpeg",
+          }}
+          style={styles.image}
+        />
       </View>
     </ScrollView>
   );
@@ -23,22 +47,9 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
-    gap: 20,
-    paddingVertical: 68,
-    paddingHorizontal: 20,
   },
-  box: {
-    height: 260,
-    width: 200,
-    borderRadius: 24,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "black",
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: "bold",
-    color: "white",
+  image: {
+    height: height,
+    width: width,
   },
 });
